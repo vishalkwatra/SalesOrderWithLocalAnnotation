4f069447da493d6f02ba2745c799b7888028bb8d
vishalkwatra <vishal.kwatra88@gmail.com>
2020-04-04 08:01:54 +0000

    [Feature]: Sales Order with Local Annotation

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("SalesOrder.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
